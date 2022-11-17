7e77454045b1de22e19957222f5a985bc29452e3
alejandrosalc <alejandrosalc06@gmail.com>
2022-11-17 17:52:44 -0500

    un cambio

--- src/main/java/proyecto/misiontic/seguridad/Controladores/ControladorUsuario.java
+++ src/main/java/proyecto/misiontic/seguridad/Controladores/ControladorUsuario.java
@@ -6,6 +6,8 @@ import proyecto.misiontic.seguridad.Repositorios.RepositorioUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -92,5 +94,20 @@ public class ControladorUsuario {
             sb.append(String.format("%02x", b));
         }
         return sb.toString();
+    }
+
+    @PostMapping("/validar")
+    public Usuario validate(@RequestBody  Usuario infoUsuario,
+                            final HttpServletResponse response) throws IOException {
+        Usuario usuarioActual=this.miRepositorioUsuario
+                .getUserByEmail(infoUsuario.getCorreo());
+        if (usuarioActual!=null &&
+                usuarioActual.getContrasena().equals(convertirSHA256(infoUsuario.getContrasena()))) {
+            usuarioActual.setContrasena("");
+            return usuarioActual;
+        }else{
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return null;
+        }
     }
 }
--- src/main/java/proyecto/misiontic/seguridad/Repositorios/RepositorioUsuario.java
+++ src/main/java/proyecto/misiontic/seguridad/Repositorios/RepositorioUsuario.java
@@ -1,7 +1,11 @@
 package proyecto.misiontic.seguridad.Repositorios;
+import org.springframework.data.mongodb.repository.Query;
 import proyecto.misiontic.seguridad.Modelos.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RepositorioUsuario extends
         MongoRepository<Usuario,String> {
+    @Query("{'correo': ?0}")
+    public Usuario getUserByEmail(String correo);
+
 }
